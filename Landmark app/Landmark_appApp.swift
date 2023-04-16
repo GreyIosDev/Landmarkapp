5f18ffd23cde2b20fb6e8b47871ef0ee9b359bcc
grey <shadeslikegrey@gmail.com>
2023-04-16 15:03:40 +0300

    Initial Commit

---
+++ Landmark app/Landmark_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_appApp.swift
+//  Landmark app
+//
+//  Created by Grey  on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
